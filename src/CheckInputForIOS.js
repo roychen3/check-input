af4a874c6f544e05099347cca0d6c994e41a278d
Roy Chen <roy850323@gmail.com>
2022-10-15 22:49:25 +0800

    Code review

--- src/CheckInputForIOS.js
+++ src/CheckInputForIOS.js
@@ -7,7 +7,6 @@ const re = new RegExp(
 
 const CheckInputForIOS = () => {
   const [value, setValue] = useState('');
-  const [isKedown, setIsKedown] = useState(false);
   const [keyDownKey, setKeyDownKey] = useState('');
   const [isComposing, setIsComposing] = useState(false);
 
@@ -15,41 +14,25 @@ const CheckInputForIOS = () => {
     if (isComposing) {
       setValue(event.target.value);
     } else {
-      setValue((prevValue) => {
-        if (
-          isKedown &&
-          (keyDownKey.match(re) || event.target.value.match(re))
-        ) {
-          if (prevValue.length - 1 === event.target.value.length) {
-            return prevValue;
-          }
-
-          if (keyDownKey.match(re) && event.target.value.match(re)) {
-            const filteredValue = event.target.value.replaceAll(re, '');
-            return filteredValue;
-          }
-
-          const filteredValue = event.target.value.replaceAll(re, '');
-          return filteredValue;
-        } else {
-          const filteredValue = event.target.value.replaceAll(re, '');
-          return filteredValue;
-        }
-      });
+      if (
+        !(
+          keyDownKey.match(re) && value.length - 1 === event.target.value.length
+        )
+      ) {
+        const filteredValue = event.target.value.replaceAll(re, '');
+        setValue(filteredValue);
+      }
     }
   };
 
   const handleKeyDown = (event) => {
-    setIsKedown(true);
     setKeyDownKey(event.key);
-  };
-  const handleKeyUp = () => {
-    setIsKedown(false);
   };
 
   const handleCompositionStart = () => {
     setIsComposing(true);
   };
+
   const handleCompositionEnd = (event) => {
     const filteredValue = event.data.replaceAll(re, '');
     setValue((prev) => prev.replace(event.data, filteredValue));
@@ -64,7 +47,6 @@ const CheckInputForIOS = () => {
         value={value}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
-        onKeyUp={handleKeyUp}
         onCompositionStart={handleCompositionStart}
         onCompositionEnd={handleCompositionEnd}
       />
